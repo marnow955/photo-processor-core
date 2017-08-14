a3a3394045da8ac1c5a67202b3bfd683bb162e33
Marek Noworolnik <marnow955@gmail.com>
2017-08-14 13:50:31 +0200

    Add Rotate and Flip

---
+++ src/main/java/photo/processor/core/Flip.java
@@ -0,0 +1,35 @@
+package photo.processor.core;
+
+import java.awt.image.BufferedImage;
+
+public class Flip extends PhotoProcessor {
+
+    public enum Orientation {
+        HORIZONTAL, VERTICAL
+    }
+
+    private Orientation orientation;
+
+    public Flip(Orientation orientation) {
+        this.orientation = orientation;
+    }
+
+    @Override
+    protected void transform() {
+        int originX;
+        int originY;
+        for (int y = 0; y < height; y++) {
+            for (int x = 0; x < width; x++) {
+                if (orientation == Orientation.HORIZONTAL) {
+                    originX = width - 1 - x;
+                    originY = y;
+                } else {
+                    originX = x;
+                    originY = height - 1 - y;
+                }
+                int rgb = image.getRGB(originX, originY);
+                resultImg.setRGB(x, y, rgb);
+            }
+        }
+    }
+}
---
+++ src/main/java/photo/processor/core/Rotate.java
@@ -0,0 +1,44 @@
+package photo.processor.core;
+
+import java.awt.image.BufferedImage;
+
+public class Rotate extends PhotoProcessor {
+
+    public enum Direction {
+        RIGHT, LEFT;
+    }
+
+    private int resultWidth;
+    private int resultHeight;
+    private Direction direction;
+
+    public Rotate(Direction direction) {
+        this.direction = direction;
+    }
+
+    @Override
+    protected void init(BufferedImage image) {
+        super.init(image);
+        resultHeight = width;
+        resultWidth = height;
+    }
+
+    @Override
+    protected void transform() {
+        int originX;
+        int originY;
+        for (int y = 0; y < resultHeight; y++) {
+            for (int x = 0; x < resultWidth; x++) {
+                if (direction == Direction.RIGHT) {
+                    originX = y;
+                    originY = height - 1 - x;
+                } else {
+                    originX = width - 1 - y;
+                    originY = x;
+                }
+                int rgb = image.getRGB(originX, originY);
+                resultImg.setRGB(x, y, rgb);
+            }
+        }
+    }
+}
